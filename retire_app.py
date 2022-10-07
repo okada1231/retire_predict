dc775d9a68fdceecdd633f2c88445e61e476f39b
okada1231 <95010965+okada1231@users.noreply.github.com>
2022-10-07 16:08:03 +0900

    Update retire_app.py

--- retire_app.py
+++ retire_app.py
@@ -4,6 +4,7 @@
 from itertools import chain
 from pyexpat import features
 from turtle import dot
+from tkinter import scrolledtext
 import numpy as np
 import pandas as pd 
 import streamlit as st
@@ -220,9 +221,6 @@ def main():
 
             # セッションステートに退避していたデータフレームを復元
             df = copy.deepcopy(st.session_state.df)
-
-            # 要約統計量の表示
-            st_display_table(df.describe())
             
         else:
             st.subheader('訓練用データをアップロードしてください')
@@ -236,14 +234,6 @@ def main():
             # セッションステートに退避していたデータフレームを復元
             df = copy.deepcopy(st.session_state.df)
 
-            # グラフの表示
-            act = ["退職", "月給(ドル)"]
-            ch = st.sidebar.selectbox("グラフのx軸", act)
-            if ch == "退職":
-                st_display_graph(df, str("退職"))
-            if ch == "月給(ドル)":
-                st_display_graph(df, str("月給(ドル)"), )
-            
         else:
             st.subheader('訓練用データをアップロードしてください')
 
@@ -254,55 +244,6 @@ def main():
 
             # セッションステートに退避していたデータフレームを復元
             df = copy.deepcopy(st.session_state.df)
-
-            # 説明変数と目的変数の設定
-            X = df.drop("退職", axis=1)   # 退職列以外を説明変数にセット
-            y = df["退職"]                # 退職列を目的変数にセット
-
-            # 決定木による予測
-            num = st.sidebar.number_input('深さ', min_value=1, max_value=3)
-            method = ["決定木", "ランダムフォレスト"]
-            learning = st.sidebar.selectbox("学習手法", method)
-            
-            if learning == "決定木":
-                clf, tra_pred_Y, val_pred_Y, acc_scores_tra, acc_scores_val, rec_scores_tra, rec_scores_val, pre_scores_tra, pre_scores_val = ml_dtree(X, y, num)
-
-                # 決定木のツリーを出力
-                st_display_dtree(clf, list(X))
-
-                # 正解率、再現率、適合率を表示
-                st.header('訓練用データでの予測精度')
-                acc_tra, rec_tra, pre_tra = st.columns(3)
-
-                with acc_tra:
-                    st.write('正解率')
-                    st.subheader(round(acc_scores_tra, 5))
-
-                with rec_tra:
-                    st.write('再現率')
-                    st.subheader(round(rec_scores_tra, 5))
-
-                with pre_tra:
-                    st.write('適合率')
-                    st.subheader(round(pre_scores_tra, 5))
-            
-                st.header('検証用データでの予測精度')
-                acc_val, rec_val, pre_val = st.columns(3)
-
-                with acc_val:
-                    st.write('正解率')
-                    st.subheader(round(acc_scores_val, 5))
-
-                with rec_val:
-                    st.write('再現率')
-                    st.subheader(round(rec_scores_val, 5))
-
-                with pre_val:
-                    st.write('適合率')
-                    st.subheader(round(pre_scores_val, 5))
-
-            if learning == "ランダムフォレスト":
-                s
 
         else:
             st.subheader('訓練用データをアップロードしてください')
