c6079f35b3a9952e9671aef6b0a5f6d2b41241a8
okada1231 <95010965+okada1231@users.noreply.github.com>
2022-10-07 16:10:18 +0900

    Update retire_app.py

--- retire_app.py
+++ retire_app.py
@@ -3,8 +3,7 @@
 
 from itertools import chain
 from pyexpat import features
-from turtle import dot
-from tkinter import scrolledtext
+
 import numpy as np
 import pandas as pd 
 import streamlit as st
